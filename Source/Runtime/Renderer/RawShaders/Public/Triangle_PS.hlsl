3b00b2e2608a20f4a9d61a9d71663d95401333a1
Benjamin Frohlund <benjamin.frohlund@hacksawstudios.com>
2025-06-13 20:14:20 +0200

    Added vertex buffer to vulkan renderer, still wip for full implementation

--- Source/Runtime/Renderer/RawShaders/Public/Triangle_PS.hlsl
+++ Source/Runtime/Renderer/RawShaders/Public/Triangle_PS.hlsl
@@ -2,10 +2,10 @@
 struct PSInput
 {
     float4 Pos : SV_POSITION;
-    float3 Color : COLOR0;
+    float4 Color : COLOR0;
 };
 
 float4 main(PSInput input) : SV_TARGET
 {
-    return float4(input.Color, 1.0f); // Output interpolated vertex color
+    return input.Color;
 }
